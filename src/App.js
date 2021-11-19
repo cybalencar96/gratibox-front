38a7a25a7b2a31fa9ec5d2ca90bcf2b4168ca775
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-11-19 09:08:38 -0300

    add layout plans page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Login from "./components/pages/Login/Login";
+import Plans from "./components/pages/Plans/Plans";
 import Signup from "./components/pages/Signup/Signup";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
                 <Route path='/' element={ <Home /> } />
                 <Route path='/login' element={ <Login /> } />
                 <Route path='/signup' element={ <Signup /> } />
+                <Route path='/plans' element={ <Plans /> } />
             </Routes>
         </BrowserRouter>
     )
